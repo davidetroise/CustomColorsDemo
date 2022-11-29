ec1564ff42e1f81b59a768e14a8af6efacae3cde
Davide Troise <davide@troise.net>
2022-11-29 11:52:56 +0100

    Initial Commit

---
+++ CustomColorsDemo/CustomColorsDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  CustomColorsDemoApp.swift
+//  CustomColorsDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct CustomColorsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
